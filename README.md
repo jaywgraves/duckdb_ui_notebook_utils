2e0171cda5eb738da5e1e4c15c32de564c6d8552
jay graves <jaywgraves@gmail.com>
2026-02-15 10:56:27 -0600

    initial scripts and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 utilities for dumping and search the notebooks from the internal duckdb ui database
+
+
+currently only tested on MacOS but could easily be tweaked for Linux
+(and I'll be working on that)
+
+
+I like to make aliases to make this easier to use.
+
+```shell
+alias duckdb_notebooks_export=~/duckdb_notebooks/duckdb_notebooks_export.sh
+alias duckdb_notebooks_search=~/duckdb_notebooks/duckdb_notebooks_search.sh
+```
+
+
+
+requirements
+----
+duckdb
+jq
+[version](https://github.com/jaywgraves/version)  this just makes a backup each time you export the notebooks.  It can easily be commented out.
---
+++ duckdb_notebooks_export.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR" || { echo "Error: Could not change to script directory."; exit 1; }
+
+# check to see if duckdb ui session is already running and exit
+# a running session will hold a lock on the `ui.db` file and the sql export command below will fail
+# leaving behind an empty `duckdb_notebooks.json` file which will cause `duckdb_notebooks_search.sh` to fail
+lsof_output=$(lsof -t "/Users/g274496/.duckdb/extension_data/ui/ui.db" 2>/dev/null)
+
+if [ -n "$lsof_output" ]; then
+    echo "Error: duckdb -ui session is already running.  stop that process to release the lock on the notebook database"
+    exit 1
+fi
+
+duckdb -readonly -json -s "select notebook_id,title,created,json_extract(json_extract(json::JSON,'cells'),'$') as jsoncells from notebook_versions where expires is null;" ~/.duckdb/extension_data/ui/ui.db > duckdb_notebooks.json
+
+# version -u $(openssl md5 duckdb_notebooks.json | awk '{print $2}') duckdb_notebooks.json
+# mv duckdb_notebooks.*.json archive
+
+version duckdb_notebooks.json
+mv duckdb_notebooks.*.*.json archive
